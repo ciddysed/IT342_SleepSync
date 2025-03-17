8b6fd9f2df2023d49ced98a74bfef70accf418ae
LAB22_USER ON WS14 <L22X07W14@citulab.net>
2025-03-17 08:08:45 +0800

    Creating folders

---
+++ SleepSync_Backend/src/main/java/com/IT342/sleepsync/SleepsyncApplication.java
@@ -0,0 +1,13 @@
+package com.IT342.sleepsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleepsyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SleepsyncApplication.class, args);
+	}
+
+}
